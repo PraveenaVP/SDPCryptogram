ebd4c4df92f74cefe56cc51ab95f679d59c97cdb
zwang635 <zwang635@gatech.edu>
2017-07-07 13:19:33 -0400

    Added server utility class

---
+++ GroupProject/SDPCryptogram/app/src/main/java/edu/gatech/seclass/sdpcryptogram/ServerUtility.java
@@ -0,0 +1,36 @@
+package edu.gatech.seclass.sdpcryptogram;
+
+import android.os.Bundle;
+import android.view.View;
+
+
+public class ServerUtility {
+
+    private void sendPlayerRatings(View view) {
+
+    }
+
+    private void sendCryptogram(View view) {
+
+    }
+
+    private void getUniqueID(View view) {
+
+    }
+
+    private void requestCryptogram(View view) {
+
+    }
+
+    private void requestCurrentPlayerRatings(View view) {
+
+    }
+
+    private void requestPlayerID(View view) {
+
+    }
+
+    private void saveCryptogram() {
+
+    }
+}
