57d4639ab0569699790c3a8e53e31519e0ba4944
pvp7 <pvp7@gatech.edu>
2017-07-12 16:55:37 -0400

    Adding new class for Cryptogram displays

---
+++ GroupProject/SDPCryptogram/app/src/main/java/edu/gatech/seclass/utilities/Cryptograms.java
@@ -0,0 +1,80 @@
+package edu.gatech.seclass.utilities;
+
+/**
+ * Created by praveena on 7/12/17.
+ */
+
+public class Cryptograms {
+    private String id;
+    private String cryptogram;
+    private String solution;
+    private String status;
+    private String incorrectcount;
+    private String userText;
+
+    //used for display the list of cryptograms
+    public Cryptograms(String id, String cryptogram, String status, String incorrectcount) {
+        this.id = id;
+        this.cryptogram = cryptogram;
+        this.status = status;
+        this.incorrectcount = incorrectcount;
+    }
+
+    public Cryptograms(String id, String cryptogram, String solution, String status, String userText) {
+        this.id = id;
+        this.cryptogram = cryptogram;
+        this.solution = solution;
+        this.status = status;
+        this.userText = userText;
+    }
+
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCryptogram() {
+        return cryptogram;
+    }
+
+    public void setCryptogram(String cryptogram) {
+        this.cryptogram = cryptogram;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getIncorrectcount() {
+        return incorrectcount;
+    }
+
+    public void setIncorrectcount(String incorrectcount) {
+        this.incorrectcount = incorrectcount;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+    }
+
+    public String getUserText() {
+        return userText;
+    }
+
+    public void setUserText(String userText) {
+        this.userText = userText;
+    }
+}
