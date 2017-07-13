f049de555a3e7813db5232768584eddcf6260311
pvp7 <pvp7@gatech.edu>
2017-07-12 22:43:15 -0400

    Updating the UI display

--- GroupProject/SDPCryptogram/app/src/main/java/edu/gatech/seclass/sdpcryptogram/View_Prior.java
+++ GroupProject/SDPCryptogram/app/src/main/java/edu/gatech/seclass/sdpcryptogram/View_Prior.java
@@ -1,16 +1,25 @@
 package edu.gatech.seclass.sdpcryptogram;
 
+import android.content.Context;
 import android.content.Intent;
+import android.icu.text.AlphabeticIndex;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import edu.gatech.seclass.utilities.CryptogramsUtil;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,12 +34,14 @@ public class View_Prior extends AppCompatActivity {
         sdpdb = new DBHelper(this);
         Bundle bundle = getIntent().getExtras();
         username= bundle.getString("username");
-        String[] testdata = sdpdb.displayPriorGames(username);
-        if(testdata != null) {
-            ListView priortList = (ListView) findViewById(R.id.prior_listview);
-            ArrayAdapter<String> priorlistAdapter = new ArrayAdapter<String>(this, R.layout.prior_list, R.id.prior_text, testdata);
-            priortList.setAdapter(priorlistAdapter);
-            priortList.setOnItemClickListener(new ItemList());
+        //String[] testdata = sdpdb.displayPriorGames(username);
+        ArrayList<CryptogramsUtil> priorlist= sdpdb.displayPriorGames1(username);
+        ListView lvpriortList = (ListView) findViewById(R.id.prior_listview);
+
+        if(priorlist != null) {
+            PriorAdapter cryptogramAdapter = new PriorAdapter(this,R.layout.prior_list,priorlist);
+            lvpriortList.setAdapter(cryptogramAdapter);
+            lvpriortList.setOnItemClickListener(new ItemList());
         }
         else
         {
@@ -39,20 +50,71 @@ public class View_Prior extends AppCompatActivity {
 
     }
 
+    class PriorAdapter extends ArrayAdapter<CryptogramsUtil>{
+        private Context mcontext;
+        private int mresource;
+
+
+        /**
+         * Constructor
+         *
+         * @param context  The current context.
+         * @param resource The resource ID for a layout file containing a TextView to use when
+         *                 instantiating views.
+         * @param objects  The objects to represent in the ListView.
+         */
+        public PriorAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull ArrayList<CryptogramsUtil> objects) {
+            super(context, resource, objects);
+            mresource = resource;
+            mcontext = context;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            String id = getItem(position).getId();
+            String puzzle = getItem(position).getCryptogram();
+            String userText = getItem(position).getUserText();
+            String status = getItem(position).getStatus();
+
+
+            CryptogramsUtil priorcryptogram = new CryptogramsUtil(id,puzzle,userText,status,true);
+
+            LayoutInflater inflator = LayoutInflater.from(mcontext);
+            convertView = inflator.inflate(mresource,parent,false);
+
+            TextView tvId = (TextView)convertView.findViewById(R.id.tvid);
+            TextView tvpuzzle = (TextView)convertView.findViewById(R.id.tvcrypt);
+            TextView tvuserText = (TextView)convertView.findViewById(R.id.tvusertext);
+            TextView tvStatus = (TextView)convertView.findViewById(R.id.tvstatus);
+
+            tvId.setText(id);
+            tvpuzzle.setText(puzzle);
+            tvuserText.setText(userText);
+            tvStatus.setText(status);
+
+
+            return convertView;
+
+
+
+
+        }
+    }
+
     class ItemList implements AdapterView.OnItemClickListener {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             ViewGroup viewg = (ViewGroup) view;
-            TextView prior_text = (TextView) viewg.findViewById(R.id.prior_text);
-            String selected = prior_text.getText().toString();
+            TextView tvUserText = (TextView) viewg.findViewById(R.id.tvusertext);
+            TextView tvcryptID=(TextView)viewg.findViewById(R.id.tvid);
+            TextView tvcrypt=(TextView)viewg.findViewById(R.id.tvcrypt);
+            int cryptID = Integer.parseInt (tvcryptID.getText().toString());
+            String crypt  = tvcrypt.getText().toString();
+            String userText = tvUserText.getText().toString();
 
-            String[] userselectedlist = selected.split("\\s*:\\s*");
-            int cryptID = Integer.parseInt(userselectedlist[0]);
-            String crypt = userselectedlist[1].toString();
-            String userText = userselectedlist[2].toString();;
+            Intent newIntent = new Intent(View_Prior.this, Solve_Prior.class);
 
-           Intent newIntent = new Intent(View_Prior.this, Solve_Prior.class);
-
-           Bundle bundle = new Bundle();
+            Bundle bundle = new Bundle();
             bundle.putString("cryptogram", crypt);
             bundle.putInt("cryptID", cryptID);
             bundle.putString("username",username);
